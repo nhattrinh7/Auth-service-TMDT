0cbc8c6dad02318ba355e9bc8f4c75f29e9871b3
Trịnh Minh Nhật <trinhminhnhatym@gmail.com>
2026-03-12 19:42:08 +0700

    modify base-retry-consumer

--- src/application/event-handlers/user-created.event-handler.ts
+++ src/application/event-handlers/user-created.event-handler.ts
@@ -1,6 +1,6 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
 import { UserCreatedEvent } from '~/domain/events/user-created.event'
-import { Inject,  } from '@nestjs/common'
+import { Inject } from '@nestjs/common'
 import { type IMessagePublisher, MESSAGE_PUBLISHER } from '~/domain/contracts/message-publisher.interface'
 
 @EventsHandler(UserCreatedEvent)
